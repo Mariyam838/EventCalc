d30621a2f90d62888f70ec86cddbd02137da2529
Mariyam838 <mariyamaashraf@gmail.com>
2025-04-13 22:25:21 +0400

    3rd commit

---
+++ EventCalc/app/src/main/java/com/example/eventcalc/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.eventcalc;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ EventCalc/app/src/main/java/com/example/eventcalc/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.eventcalc;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Optional: delay and move to main activity
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000); // 2-second splash
+    }
+}
+
